f9802edadf4ae924b7ad75f318c4caffe1ebd3c1
root <root@8083b509234d>
2025-01-24 15:04:42 +0530

    Upload file test_home_page.py

---
+++ test_home_page.py
@@ -0,0 +1,17 @@
+import pytest
+from pages.home_page import Edit
+@pytest.mark.usefixtures("setup")
+class TestEdit:
+    def test_to_verify_the_two_step_verification(self):
+        # Initialize Edit class
+        edit_driver = Edit(self.driver)
+
+        # Open login page
+        edit_driver.open_login_page()
+
+        # Use the dynamically passed username and password
+        edit_driver.enter_username(self.username)
+        edit_driver.enter_password(self.password)
+
+        # Click on the sign-in button
+        edit_driver.clicking_on_sign_in()
