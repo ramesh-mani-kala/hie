70388a807d5bd95180be6371d52f8fca55534fbf
root <root@8083b509234d>
2025-01-27 14:39:17 +0530

    Pushed Successfully

--- test.py
+++ test.py
@@ -6,9 +6,10 @@ commands = ['start', 'stop', 'pause', 'resume', 'exit']
 # Function to implement autosuggestions
 def completer(text, state):
     # Return the list of possible commands that start with the text typed by the user
-    options = [cmd for cmd in commands if cmd.starts  with(text)]
+    options = [cmd for cmd in commands if cmd.startswith(text)]
     try:
         return options[state]
+      
     except IndexError:
         return None
 
